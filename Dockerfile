8eeaa0b2af7dc8310e4b2830315f23b4b8c126f8
edgd1er <edgd1er@hotmail.com>
2020-10-19 18:22:45 +0200

    simplified start.sh, sqlite database is a parameter, multi arch build through build

---
+++ Dockerfile
@@ -0,0 +1,43 @@
+# Switch to the target image
+FROM alpine:3.12
+
+ENV IP		    0.0.0.0
+ENV PORT		  1688
+ENV EPID		  ""
+ENV LCID		  1033
+ENV SQLITE    false
+ENV HWID		  "364F463A8863D35F"
+ENV LOGLEVEL	ERROR
+ENV LOGFILE   /var/log/pykms_logserver.log
+ENV LOGSIZE	  ""
+ENV CLIENT_COUNT	26
+ENV ACTIVATION_INTERVAL	120
+ENV RENEWAL_INTERVAL	10080
+ENV PYKMS_DB      /db/pykms_database.db
+
+COPY docker/docker-py3-kms/start.sh /usr/bin/start.sh
+COPY py-kms /home/py-kms
+
+RUN apk add --no-cache --update \
+	bash \
+	git \
+	py3-argparse \
+	py3-flask \
+	py3-pygments \
+	python3-tkinter \
+	sqlite-libs \
+	py3-pip && \
+    git clone https://github.com/coleifer/sqlite-web.git /tmp/sqlite_web && \
+    mv /tmp/sqlite_web/sqlite_web /home/ && \
+    rm -rf /tmp/sqlite_web && \
+    pip3 install peewee tzlocal pysqlite3 && \
+    mkdir /db/ && \
+    chmod a+x /usr/bin/start.sh && \
+    apk del git
+
+WORKDIR /home/py-kms
+
+EXPOSE ${PORT}/tcp
+EXPOSE 8080
+
+ENTRYPOINT ["/usr/bin/start.sh"]
--- docker/docker-py3-kms/start.sh
+++ docker/docker-py3-kms/start.sh
@@ -1,50 +1,25 @@
 #!/bin/bash
+# EN: Start daemons
+# RU: Запуск демонов
+set -x
+
 cd /home/py-kms
-if [ "$SQLITE" == false ];
-then
-  if [ "$EPID" == "" ];
-  then
-    if [ "$LOGSIZE" == "" ];
-    then
-      /usr/bin/python3 pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE}
-    else
-      /usr/bin/python3 pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE}	-S ${LOGSIZE}
-    fi
-  else
-    if [ "$LOGSIZE" == "" ];
-    then
-      /usr/bin/python3 pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE}
-    else
-      /usr/bin/python3 pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} -S ${LOGSIZE}
-    fi
-  fi
+
+CMD1="/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -w ${HWID} -V ${LOGLEVEL}"
+
+[[ "$SQLITE" != false ]] && PYARGS+=" -s ${PYKMS_DB}"
+
+[ "$EPID" != "" ] && PYARGS+=" -e ${EPID}"
+
+[ "$LOGSIZE" == "" ] && PYARGS+= " -S ${LOGSIZE}"
+
+CMDALL="${CMD1} ${PYARGS}"
+
+if [ "${SQLITE}" != false ]; then
+  /bin/bash -c "$CMDALL &"
+  sleep 5
+  /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
+  /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PYKMS_DB} --read-only
 else
-  if [ "$EPID" == "" ];
-  then
-    if [ "$LOGSIZE" == "" ];
-    then
-      /bin/bash -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s ${PWD}/pykms_database.db -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} &"
-      sleep 5
-      /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/pykms_database.db --read-only
-    else
-      /bin/bash -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s ${PWD}/pykms_database.db -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} -S ${LOGSIZE} &"
-      sleep 5
-      /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/pykms_database.db --read-only
-    fi
-  else
-    if [ "$LOGSIZE" == "" ];
-    then
-      /bin/bash -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s ${PWD}/pykms_database.db -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} &"
-      sleep 5
-      /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/pykms_database.db --read-only
-    else
-      /bin/sh -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s ${PWD}/pykms_database.db -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} -S ${LOGSIZE} &"
-      sleep 5
-      /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/pykms_database.db --read-only
-    fi
-  fi
+  $CMDALL
 fi
