776dcbaf2d4862a0822bd6e9f71b5e01a4f05664
edgd1er <edgd1er@hotmail.com>
2021-10-15 01:47:18 +0200

    lint dockerfile, set TZ and UID/GID, have client log.

--- docker/entrypoint.py
+++ docker/entrypoint.py
@@ -1,54 +1,45 @@
 #!/usr/bin/python3
 
-# This replaces the old start.sh and ensures all arguments are bound correctly from the environment variables...
+# Need root privileges to change timezone, and user uid/gid
 
+import grp
 import os
-import time
+import pwd
 import subprocess
 
-argumentVariableMapping = {
-    '-l': 'LCID',
-    '-c': 'CLIENT_COUNT',
-    '-a': 'ACTIVATION_INTERVAL',
-    '-r': 'RENEWAL_INTERVAL',
-    '-w': 'HWID',
-    '-V': 'LOGLEVEL',
-    '-F': 'LOGFILE',
-    '-S': 'LOGSIZE',
-    '-e': 'EPID'
-}
 
-# Build the command to execute
-command = ['/usr/bin/python3', 'pykms_Server.py', os.environ.get('IP'), os.environ.get('PORT')]
-for (arg, env) in argumentVariableMapping.items():
-    if env in os.environ and os.environ.get(env) != '':
-        command.append(arg)
-        command.append(os.environ.get(env))
-        
-enableSQLITE = os.environ.get('SQLITE').lower() == 'true'
-os.makedirs('db', exist_ok=True)
-dbPath = os.path.join('db', 'pykms_database.db')
-if enableSQLITE:
-    print('Storing database file to ' + dbPath)
-    command.append('-s')
-    command.append(dbPath)
+def change_uid_grp():
+  user_db_entries = pwd.getpwnam("py-kms")
+  user_grp_db_entries = grp.getgrnam("power_users")
+  uid = user_db_entries.pw_uid
+  gid = user_grp_db_entries.gr_gid
+  new_gid = int(os.getenv('GID', str(gid)))
+  new_uid = int(os.getenv('UID', str(uid)))
+  os.chown("/home/py-kms", new_uid, new_uid)
+  os.chown("/db/pykms_database.db", new_uid, new_uid)
+  if gid != new_gid:
+    print("Setting gid to " + str(new_gid), flush=True)
+    os.setgid(gid)
+  if uid != new_uid:
+    print("Setting uid to " + str(new_uid), flush=True)
+    os.setuid(uid)
 
-pykmsProcess = subprocess.Popen(command)
 
-# In case SQLITE is defined: Start the web interface
-if enableSQLITE:
-    time.sleep(5) # The server may take a while to start
-    if not os.path.isfile(dbPath):
-        # Start a dummy activation to ensure the database file is created
-        subprocess.run(['/usr/bin/python3', 'pykms_Client.py', os.environ.get('IP'), os.environ.get('PORT'), '-m', 'Windows10', '-n', 'DummyClient', '-c', 'ae3a27d1-b73a-4734-9878-70c949815218'])
-    sqliteProcess = subprocess.Popen(['/usr/bin/python3', '/home/sqlite_web/sqlite_web.py', '-H', os.environ.get('IP'), '--read-only', '-x', dbPath, '-p', os.environ.get('SQLITE_PORT')])    
+def change_tz():
+  tz = os.getenv('TZ', 'etc/UTC')
+  # TZ is not symlinked and defined TZ exists
+  if tz not in os.readlink(LTIME) and os.path.isfile('/usr/share/zoneinfo/' + tz):
+    print("Setting timezone to " + tz, flush=True)
+    os.remove(LTIME)
+    os.symlink(os.path.join('/usr/share/zoneinfo/', tz), LTIME)
 
-try:
-    pykmsProcess.wait()
-except:
-    # In case of any error - just shut down
-    pass
 
-if enableSQLITE:
-    sqliteProcess.terminate()
-    pykmsProcess.terminate()
+LTIME = '/etc/localtime'
+PYTHON3 = '/usr/bin/python3'
+log_level = os.getenv('LOGLEVEL', 'INFO')
+
+# Main
+if (__name__ == "__main__"):
+  change_tz()
+  change_uid_grp()
+  subprocess.call("/usr/bin/start.py",shell=True)
